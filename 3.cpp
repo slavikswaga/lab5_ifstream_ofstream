154c5582cdd75611da32f8938a24a4b2c26596b0
slavikswaga <kustovslava97@gmail.com>
2026-05-23 03:16:41 +0700

    task 3 modify

--- 3.cpp
+++ 3.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 struct Sale{
     std::string date_of_sale;
@@ -36,7 +37,16 @@ void write_func(){
 }
 
 void read_func(){
-    std::ifstream fin("sales.csv");
+    std::string file = "";
+    std::cout << "Input the file you want to read" << std::endl;
+    std::cout << "sales.csv" << std::endl;
+    std::cout << "filtered_sales.csv" << std::endl;
+    std::cin >> file;
+    std::ifstream fin(file);
+    if(!fin.is_open()){
+        std::cout << "File not found!" << std::endl;
+        return;
+    }
     std::string line;
     while(std::getline(fin, line)){
         std::stringstream ss(line);
@@ -53,9 +63,13 @@ void read_func(){
     return;
 }
 
-double calculate_total_sum(){
+void calculate_total_sum(){
     double total_sum = 0;
     std::ifstream fin("sales.csv");
+    if(!fin.is_open()){
+        std::cout << "File not found!" << std::endl;
+        return;
+    }
     std::string line;
     while(std::getline(fin, line)){
         std::stringstream ss(line);
@@ -69,10 +83,79 @@ double calculate_total_sum(){
         total_sum += s.sum;
     }
     fin.close();
-    return total_sum;
+    std::cout << "total sales amount = " << total_sum << std::endl;
+    return;
+}
+
+void write_filtered_sales(){
+    std::string necessary_date = "";
+    std::cout << "Input necessary date for filtering sales\n";
+    std::cout << "format - 'dd.mm.yyyy'\n";
+    std::cin >> necessary_date;
+    std::vector<Sale> filtered;
+    std::ofstream fout("filtered_sales.csv", std::ios::app);
+    std::ifstream fin("sales.csv");
+    if(!fin.is_open()){
+        std::cout << "File not found!" << std::endl;
+        return;
+    }
+    if(!fout.is_open()){
+        std::cout << "Failed opening file!" << std::endl;
+        return;
+    }
+    std::string line;
+    
+    while(std::getline(fin, line)){
+        Sale s;
+        std::stringstream ss(line);
+        std::getline(ss, s.date_of_sale, ',');
+        ss >> s.num_of_buyer;
+        ss.ignore();
+        ss >> s.sum;
+        ss.ignore();
+        std::getline(ss, s.product_category);
+        if(s.date_of_sale == necessary_date){
+            filtered.push_back(s);
+        }
+    }
+    for(int i = 0; i < filtered.size(); i++){
+        fout << filtered[i].date_of_sale << "," << filtered[i].num_of_buyer << "," << filtered[i].sum << "," << filtered[i].product_category << std::endl;
+    }
+    fout.close();
+    fin.close();
+    return;
 }
 int main(){
-
-    read_func();
+    int choise = 0;
+    std::cout << "Input command number:" << std::endl;
+    std::cout << "1 - if you want write sales\n";
+    std::cout << "2 - if you want read info about sales\n";
+    std::cout << "3 - if you want to write the data first and then read it\n";
+    std::cout << "4 - if you want to filter sales for date\n";
+    std::cout << "5 - if you want to print the total cost of all sales\n";
+    std::cin >> choise;
+    switch (choise)
+    {
+    case 1:
+        write_func();
+        break;
+    case 2:
+        read_func();
+        break;
+    case 3:
+        write_func();
+        read_func();
+        break;
+    case 4:
+        write_filtered_sales();
+        read_func();
+        break;
+    case 5:
+        calculate_total_sum();
+        break;
+    default:
+        std::cout << "Unknown command\n";
+        break;
+    }
     return 0;
 }
